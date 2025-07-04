ee9f722ff4fb167707f7848b61927b1263b87ab2
MRizainF <icon.firdaus@gmail.com>
2025-07-04 18:16:55 +0700

    refactor: route to loading page

--- src/landing-page/hero.tsx
+++ src/landing-page/hero.tsx
@@ -1,8 +1,19 @@
+'use client';
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Spotlight } from "@/components/ui/spotlight-new";
 
 const Hero = () => {
+  const router = useRouter();
+
+  const handleGetStarted = () => {
+    // Redirect ke halaman loading terlebih dahulu
+    router.push("/loading?redirect=/auth/login");
+  };
+
   return (
     <div className="guide-main h-screen flex flex-col items-center justify-center">
+        <Spotlight></Spotlight>
         <p className="title uppercase text-center px-50 shiny-text">
             invest on land or water with ease and secure
         </p>
@@ -10,8 +21,13 @@ const Hero = () => {
             invest easily using any cryptocurrency and helping you to secure your transaction using web3 technology
         </p>
         <div className="flex gap-6">
-            <button className="mt-10 button-main hover-bg-glow uppercase px-6 py-4 cursor-pointer hover:scale-105 transform">Get Started</button>
-            <button className="mt-10 button-submain uppercase px-6 py-4 font-base cursor-pointer">Learn More</button>
+            <button 
+                onClick={handleGetStarted}
+                className="mt-10 button-main hover-bg-glow uppercase px-10 py-3 cursor-pointer hover:scale-105 transform"
+            >
+                Get Started
+            </button>
+            <button className="mt-10 button-submain uppercase px-10 py-3 font-base cursor-pointer">Learn More</button>
         </div>
         <Image 
             src="/landing-page/bg-farmer.png" 
