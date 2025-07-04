645f40ef71427ed17221ba2a5d6e302dcce0ee4f
MRizainF <icon.firdaus@gmail.com>
2025-07-04 18:57:04 +0700

    refactor: update dependencies and enhance styling for landing page components

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -9,7 +9,7 @@ export function Navbar() {
   };
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center relative z-50">
         <div className="fixed top-4 left-1/2 transform -translate-x-1/2 px-10 py-2 bg-transparent backdrop-blur-2xl border-1 border-grey-main rounded-full">
             <div className="flex gap-2 items-center text-sm text-grey-secondary">
                 <a href="" className="hover:bg-black hover:rounded-lg hover:text-white px-4 py-2">Home</a>
